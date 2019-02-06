2628a30415ba1ff0e2b8414255f1e678469be472
Hovsep <avakyan.hovsep@yandex.ru>
2019-02-06 13:00:26 +0300

    Add migrations

---
+++ database/migrations/2019_02_05_151957_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('user');
+            $table->integer('culture');
+            $table->integer('management');
+            $table->integer('work_live_balance');
+            $table->integer('career_development');
+            $table->longText('pro');
+            $table->longText('contra');
+            $table->longText('suggestions');
+            $table->timestamps();
+        });
+    }
+
+}
