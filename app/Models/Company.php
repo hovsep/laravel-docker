78acfda0747c58f9d33433098091b5c63e81f2fa
Hovsep <avakyan.hovsep@yandex.ru>
2019-02-06 16:02:52 +0300

    Add average ratings to company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -16,8 +16,35 @@ class Company extends Model
         'industry'
     ];
 
+    protected $appends = [
+        'average_culture',
+        'average_management',
+        'average_work_live_balance',
+        'average_career_development'
+    ];
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function getAverageCultureAttribute()
+    {
+        return $this->reviews()->avg('culture');
+    }
+
+    public function getAverageManagementAttribute()
+    {
+        return $this->reviews()->avg('management');
+    }
+
+    public function getAverageWorkLiveBalanceAttribute()
+    {
+        return $this->reviews()->avg('work_live_balance');
+    }
+
+    public function getAverageCareerDevelopmentAttribute()
+    {
+        return $this->reviews()->avg('career_development');
     }
 }
